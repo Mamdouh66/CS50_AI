f1852cad0fc45fb9728f18a29e48f6898a881891
Mamdouh66 <2210002016@iau.edu.sa>
2023-03-04 01:43:17 +0300

    I guess finishd test

--- shopping/test.py
+++ shopping/test.py
@@ -7,7 +7,7 @@ months = {
     'Mar': 2,
     'Apr': 3,
     'May': 4,
-    'Jun': 5,
+    'June': 5,
     'Jul': 6,
     'Aug': 7,
     'Sep': 8,
@@ -20,10 +20,35 @@ months = {
 with open('shopping.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
 
+    non_neumirc_evidence_list = []
     evidence_list = []
     labels_list = []
 
     next(csv_reader)
 
     for line in csv_reader:
-        print(line)
+        non_neumirc_evidence_list.append(line[:-1:])
+        labels_list.append(1 if line[-1:][0] == "TRUE" else 0)
+
+    for evidence in non_neumirc_evidence_list:
+        temp = []
+        ints = [0, 2, 4, 11, 12, 13, 14]
+        floats = [1, 3, 5, 6, 7, 8, 9]
+        temp.append(int(evidence[0]))
+        temp.append(float(evidence[1]))
+        temp.append(int(evidence[2]))
+        temp.append(float(evidence[3]))
+        temp.append(int(evidence[4]))
+        temp.append(float(evidence[5]))
+        temp.append(float(evidence[6]))
+        temp.append(float(evidence[7]))
+        temp.append(float(evidence[8]))
+        temp.append(float(evidence[9]))
+        temp.append(int(months[evidence[10]]))  # Months
+        temp.append(int(evidence[11]))
+        temp.append(int(evidence[12]))
+        temp.append(int(evidence[13]))
+        temp.append(int(evidence[14]))
+        temp.append(1 if evidence[15] == "Returning_Visitor" else 0)  # V_Type
+        temp.append(1 if evidence[16] == "TRUE" else 0)  # Weekend
+        evidence_list.append(temp)
